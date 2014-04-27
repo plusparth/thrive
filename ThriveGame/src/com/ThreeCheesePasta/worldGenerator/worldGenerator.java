dc2e4479959a6d8fed2b255cb883287ecbebd123
Parth Oza <plusparth@gmail.com>
2014-04-27 13:59:40 -0400

    Created pack

--- ThriveGame/src/com/ThreeCheesePasta/worldGenerator/worldGenerator.java
+++ ThriveGame/src/com/ThreeCheesePasta/worldGenerator/worldGenerator.java
@@ -23,14 +23,22 @@ public class worldGenerator {
 		}
 		return biomesInWorld;
 	}
+	
 	private int sliceSize = 128;
 	private int worldDim = sliceSize * sliceSize;
-	public int[] fullMap = new int[worldDim];
+	public int[][] fullMap = new int[sliceSize][worldDim];
+	//Generates heightmap for stuffs
 	public int[] heightMap(int first, int last, int width, int height, float displace, float roughness) {
 		int power = (int) Math.floor(Math.pow(2,Math.ceil(Math.log(width) / Math.log(2))));
 		int[] points = new int[power];
+		//Take first point into consideration
+		points[0] = first;
+		points[power] = last;
+		//If randomly generating either first or last point
 		if(first < 0) { points[0] = (int) Math.floor(height/2 + (Math.random()*displace*2) - displace); }
 		if(last < 0) { points[power] = (int) Math.floor(height/2 + (Math.random()*displace*2) - displace); }
+		
+		//Midpoint displacement
 	    displace *= roughness;
 	    for(int i = 1; i < power; i *=2) {
 	        for(int j = (power/i)/2; j < power; j+= power/i) {
@@ -38,14 +46,15 @@ public class worldGenerator {
 	            points[j] += (Math.random()*displace*2) - displace;
 	        }
 	        displace *= roughness;
-	    }    
+	    }   
+	    
 		return points;
 	}
-	public int[] addToArray(int[] originalArray, int[] adder) {
-		int x = Arrays.asList(originalArray).indexOf(null);
+	
+	//Combines arrays into the 2d thing
+	public int[][] addToArray(int[][] originalArray, int[] adder, int numOfSlice) {
 		for(int i = 0; i < adder.length; i++) {
-			originalArray[x] = adder[i];
-			x++;
+			originalArray[i][numOfSlice] = adder[i];
 		}
 		return originalArray;
 	}
